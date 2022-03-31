6afabe932b842106251e33c1b9332a269c3239cc
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-31 12:06:46 -0700

    wrote displayAllWorkshops function

--- workshops/workshops.js
+++ workshops/workshops.js
@@ -9,10 +9,35 @@ checkAuth();
 const logoutButton = document.getElementById('logout');
 const workshopsEl = document.getElementById('workshops-container');
 
-window.addEventListener('load', async () => {
+async function displayAllWorkshops() {
+    workshopsEl.textContent = '';
     const workshops = await getWorkshops();
-    console.log(workshops);
-});
+    for (let workshop of workshops) {
+        const workshopDiv = document.createElement('div');
+        const nameEl = document.createElement('h3');
+        const participantsEl = document.createElement('div');
+
+        workshopDiv.classList.add('workshop');
+        nameEl.classList.add('workshop-name');
+        participantsEl.classList.add('participants');
+
+        // nameEl.textContent = workshop;
+        
+        for (let participant of workshop.participants) {
+            const anchor = document.createElement('a');
+            const participantDiv = document.createElement('div');
+            participantDiv.classList.add('participant');
+            participantDiv.textContent = participant.name;
+            anchor.href = `../edit-participant/?id=${participant.id}`;
+            anchor.append(participantDiv);
+            participantsEl.append(anchor);
+        }
+        workshopDiv.append(nameEl, participantsEl);
+        workshopsEl.append(workshopDiv);
+    }
+}
+
+window.addEventListener('load', displayAllWorkshops());
 
 logoutButton.addEventListener('click', () => {
     logout();
