d1062c9ef39d6e17e9f46df5e5ac6e3cc679abe5
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-31 12:34:30 -0700

    fixed supabase fetch function

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,13 +1,12 @@
 const SUPABASE_URL = 'https://zhmowgcybteqgiwwrxln.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpobW93Z2N5YnRlcWdpd3dyeGxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc5OTQ2NjYsImV4cCI6MTk2MzU3MDY2Nn0.e8IeeowEcZ9C7aazuyONAepUhFvdOgDSLq8EKRJWwls';
-
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export async function getWorkshops() {
-    const response = client
+    const response = await client
         .from('workshops')
-        .select('*');
-        // .order('topic', { ascending: true });
+        .select('*, participants (*)')
+        .order('topic', { ascending: true });
 
     return checkError(response);
 }
