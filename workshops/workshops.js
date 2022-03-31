054f7069e36b2a1d13d5cc06fa69ea6207449ea4
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-31 11:53:45 -0700

    initial workshop page HTML/CSS setup

--- workshops/workshops.js
+++ workshops/workshops.js
@@ -1,8 +1,18 @@
-import { checkAuth, logout } from '../fetch-utils.js';
+import { 
+    checkAuth, 
+    logout,
+    getWorkshops
+} from '../fetch-utils.js';
 
 checkAuth();
 
 const logoutButton = document.getElementById('logout');
+const workshopsEl = document.getElementById('workshops-container');
+
+window.addEventListener('load', async () => {
+    const workshops = await getWorkshops();
+    console.log(workshops);
+});
 
 logoutButton.addEventListener('click', () => {
     logout();
