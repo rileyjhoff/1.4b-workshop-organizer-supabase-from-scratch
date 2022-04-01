4d95e739f6097c5dc5a63a98cc32e19c80102c8a
Riley Hoffman <rileyjhoff@gmail.com>
2022-04-01 10:24:05 -0700

    wrote HTML skeletons for add and edit participant pages. wrote on load event listener that renders and appends workshop options

--- add-participant/add-participant.js
+++ add-participant/add-participant.js
@@ -7,8 +7,23 @@ import {
 checkAuth();
 
 const logoutButton = document.getElementById('logout');
+const form = document.getElementById('add-participant-form');
+const nameEl = document.getElementById('name');
+const selectEl = document.getElementById('workshop-select');
 
-// window.addEventListener('load', displayAllWorkshops());
+
+
+window.addEventListener('load', renderWorkshopOptions());
+
+async function renderWorkshopOptions() {
+    const workshops = await getWorkshops();
+    for (let workshop of workshops) {
+        const optionEl = document.createElement('option');
+        optionEl.value = workshop.id;
+        optionEl.textContent = workshop.topic;
+        selectEl.append(optionEl);
+    }
+}
 
 logoutButton.addEventListener('click', () => {
     logout();
