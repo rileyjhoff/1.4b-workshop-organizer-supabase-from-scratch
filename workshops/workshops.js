d321eacef3da0a18b769c257de9aeac5ed60230d
Riley Hoffman <rileyjhoff@gmail.com>
2022-04-01 12:17:09 -0700

    new div is appended to workshops page if there are workshop-less participants

--- workshops/workshops.js
+++ workshops/workshops.js
@@ -37,6 +37,30 @@ async function displayAllWorkshops() {
         workshopDiv.append(nameEl, participantsEl);
         workshopsEl.append(workshopDiv);
     }
+    const participantsNeedingWorkshop = await getNullParticipants();
+    if (participantsNeedingWorkshop.length > 0) {
+        const workshopDiv = document.createElement('div');
+        const nameEl = document.createElement('h3');
+        const participantsEl = document.createElement('div');
+
+        workshopDiv.classList.add('workshop');
+        workshopDiv.id = 0;
+        nameEl.classList.add('workshop-name');
+        participantsEl.classList.add('participants');
+
+        nameEl.textContent = 'Need New Workshop';
+        for (let participant of participantsNeedingWorkshop) {
+            const anchor = document.createElement('a');
+            const participantDiv = document.createElement('div');
+            participantDiv.classList.add('participant');
+            participantDiv.textContent = participant.name;
+            anchor.href = `../edit-participant/?id=${participant.id}`;
+            anchor.append(participantDiv);
+            participantsEl.append(anchor);
+        }
+        workshopDiv.append(nameEl, participantsEl);
+        workshopsEl.append(workshopDiv);
+    }
 }
 
 window.addEventListener('load', async () => {
